7ba343c382c62ad43b634e7ca4ad8d6c3d522933
LennyTaelman <lenny.taelman@gmail.com>
2024-10-10 11:06:57 +0200

    stated key factorization lemma

--- LeanCondensed/Projects/LightProfiniteInjective.lean
+++ LeanCondensed/Projects/LightProfiniteInjective.lean
@@ -2,10 +2,9 @@
 Authors: Lenny Taelman
 -/
 import Mathlib.CategoryTheory.Preadditive.Injective
-import Mathlib.Condensed.Discrete.Basic
+-- import Mathlib.CategoryTheory.FinCategory
 import Mathlib.Topology.Category.LightProfinite.Sequence
-import LeanCondensed.Mathlib.Condensed.Light.Limits
-import LeanCondensed.Projects.InternallyProjective
+-- import LeanCondensed.Mathlib.Condensed.Light.Limits
 /-!
 
 # Project: show that non-empty light profinite spaces are injective in all profinite spaces
@@ -18,13 +17,33 @@ noncomputable section
 
 universe u
 
-open CategoryTheory LightProfinite OnePoint Limits LightCondensed MonoidalCategory MonoidalClosed
+open CategoryTheory LightProfinite OnePoint Limits
 
 namespace LightProfinite
+
+lemma bla (X Y : Profinite.{u}) : ∃ f : X → Y, Continuous f := sorry
+
+
+
+-- key factorization lemma in topology
+lemma to_discrete_lifts_along_injective_profinite
+  (S : Type u) [TopologicalSpace S] [DiscreteTopology S] [Nonempty S]
+  (X Y : Profinite.{u}) (f : X → Y) (f_cont: Continuous f) (f_inj: Function.Injective f)
+  (g : X → S) (g_cont : Continuous g) :
+  ∃ h : Y → S, (h ∘ f = g) ∧ (Continuous h) := by
+  -- write X and Y as compatible limits of finite sets
+
+  sorry
+
+
+
 
 -- this is the target theorem!
 theorem injective_of_light (S : LightProfinite.{u}) [Nonempty S]:
   Injective (lightToProfinite.obj S) := by
+  constructor
+  intro X Y f g h
+  -- write
   sorry
 
 end LightProfinite
