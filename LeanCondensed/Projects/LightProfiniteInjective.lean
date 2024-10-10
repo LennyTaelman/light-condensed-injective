ea2e8e0f9c6c4e9059dfd40126469313dbb426db
LennyTaelman <lenny.taelman@gmail.com>
2024-10-10 18:13:47 +0200

    messy first attempts at proving lifting lemma

--- LeanCondensed/Projects/LightProfiniteInjective.lean
+++ LeanCondensed/Projects/LightProfiniteInjective.lean
@@ -19,22 +19,34 @@ universe u
 
 open CategoryTheory LightProfinite OnePoint Limits
 
-namespace LightProfinite
-
-lemma bla (X Y : Profinite.{u}) : ∃ f : X → Y, Continuous f := sorry
 
 
 
--- key factorization lemma in topology
+-- key extension lemma in topology
 lemma to_discrete_lifts_along_injective_profinite
   (S : Type u) [TopologicalSpace S] [DiscreteTopology S] [Nonempty S]
   (X Y : Profinite.{u}) (f : X → Y) (f_cont: Continuous f) (f_inj: Function.Injective f)
   (g : X → S) (g_cont : Continuous g) :
   ∃ h : Y → S, (h ∘ f = g) ∧ (Continuous h) := by
-  -- write Y as lim Y_i
-  have Y_fin := sorry
+  -- write Y as lim Y_i with Y_i discrete
+  have Y_fin : DiscreteQuotient Y ⥤ TopCat.{u} := Y.diagram ⋙ Profinite.toTopCat
+  -- for ever i in DiscreteQuotient Y we have that Y_i is discrete
+  have Y_fin_discrete : ∀ Z : DiscreteQuotient Y, Discrete (Y_fin.obj Z) := by
+    intro Z
+
+    sorry
+
+  #check Y_fin.obj
+  #check Y_fin.map
+
 
   -- define X_i = im(X→Y→Y_i)
+  def X_fin : DiscreteQuotient Y ⥤ FintypeCat.{u} where
+    obj Z := Y_fin.obj Z
+    map f := Y_fin.map f
+    map_comp f g := sorry
+    map_id Z := sorry
+
 
   -- show X → lim X_i is iso
 
