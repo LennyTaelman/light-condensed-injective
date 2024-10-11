b8832f0d424ef715448144350b0368ced6641ed6
LennyTaelman <lenny.taelman@gmail.com>
2024-10-11 14:03:12 +0200

    messing around with morphisms in TopCat vs continuous maps

--- LeanCondensed/Projects/LightProfiniteInjective.lean
+++ LeanCondensed/Projects/LightProfiniteInjective.lean
@@ -19,15 +19,80 @@ universe u
 
 open CategoryTheory LightProfinite OnePoint Limits
 
+-- SCRATCHPAD START
+
+
+-- moving from objects in TopCat.{u} to topological spaces and back
+
+variable (X : TopCat.{u})
+
+
+#check Bundled.α X
+#check TopCat.of (Bundled.α X)
+
+def sanity : X ≅ TopCat.of (Bundled.α X) where
+  hom := 𝟙 X
+  inv := 𝟙 X
+
+-- from morphism to continuous map
+
+variable (Y : TopCat.{u}) (f : X ⟶ Y)
+#check f.toFun
+instance cont_f : Continuous f.toFun := by continuity
+
+-- from continuous map to morphism
+variable (X_0 Y_0 : Type u) [TopologicalSpace X_0] [TopologicalSpace Y_0]
+variable (g : X_0 → Y_0) [g_cont : Continuous g]
+
+
+
+-- underlying discrete space of S
+
+
+-- the projection map Y ⟶ S in TopCat
+variable (S : DiscreteQuotient Y)
+#check (S.proj : Y ⟶ (TopologicalSpace S).toTopCat)
+
+
+-- exc 1: define the composite map X ⟶ S
+def g : X ⟶ T := S.proj ∘ f
+
+
+
+
+-- implement range of f
+def fund : X → Y := f
+#check f
+#check (f: X→ Y)
+
+def range (f : X ⟶ Y) : Set Y := { y | ∃ x, f x = y }
+
+
+variable (S T : DiscreteQuotient Y) (g : S ⟶ T)
+#check S
+#check g
+#check (S : Type u)
+
+
+
+
+-- SCRATCHPAD END
+
+
 /- given continuous map f : X → Y of profinite spaces,
   with Y = lim Y_i the canonical diagram of Y,
   produce a diagram of X with X_i = im(X → Y → Y_i)
   and a map X → lim X_i compatible with f -/
 
-def diagram_of_injection {X Y : Profinite.{u}} (f : X ⟶ Y) :
-    DiscreteQuotient Y ⥤ FintypeCat.{u} where
-  obj Z := (Y.fintypeDiagram.obj Z) -- this is not yet what we want; take image!
-  map Z₁ Z₂ g := sorry
+def diagram_of_injection {X Y : Profinite.{u}} (f : X → Y) (f_cont: Continuous f) :
+    DiscreteQuotient Y ⥤ Type u where
+  obj S := Set.range (S.proj ∘ f)
+  map {S T} g := λ x ↦ g (f x)
+
+-- having difficulty formalizing this. TODO: find baby case, split up in smaller problems
+-- minimal example?
+
+
 
 /- if moreover f is injective, then the map X → lim X_i is an iso -/
 
