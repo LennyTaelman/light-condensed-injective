b39499eda841d37136d58f20fe98b59b61b14910
LennyTaelman <lenny.taelman@gmail.com>
2024-10-11 21:14:16 +0200

    reorganise approach; split out in more elementary parts

--- LeanCondensed/Projects/LightProfiniteInjective.lean
+++ LeanCondensed/Projects/LightProfiniteInjective.lean
@@ -19,125 +19,22 @@ open CategoryTheory LightProfinite Limits
 
 -- SCRATCHPAD START
 
-
--- moving from objects in TopCat.{u} to topological spaces and back
-
-variable (X : TopCat.{u})
-
-
-#check Bundled.α X
-#check TopCat.of (Bundled.α X)
-
-def sanity : X ≅ TopCat.of (Bundled.α X) where
-  hom := 𝟙 X
-  inv := 𝟙 X
-
--- from morphism to continuous map
-
-variable (Y : TopCat.{u}) (f : X ⟶ Y)
-#check f.toFun
-instance cont_f : Continuous f.toFun := by continuity
-
--- from continuous map to morphism
-variable (X_0 Y_0 : Type u) [TopologicalSpace X_0] [TopologicalSpace Y_0]
-variable (g : C(X_0, Y_0))
-#check g -- g : C(X_0, Y_0)
-#check g.toFun -- g.toFun : X_0 → Y_0
-
-#check TopCat.of X_0
-#check (g : TopCat.of X_0 ⟶ TopCat.of Y_0)
+/-
+  given F,G : I ⟶ Type and F ⟶ G,
+  prouce factorization F ⟶ im(F ⟶ G) ⟶ G
+  and induced maps on limits
+-/
 
 
--- image of a morphism in TopCat.{u}
-variable (X Y : Profinite.{u}) (f : X ⟶ Y)
--- #check (inferInstance : HasImage f)
-
--- fails to synthesize, so let's work concretely
--- goal: produce a subtype S of
-variable (X Y : Type u) [TopologicalSpace X] [TopologicalSpace Y] (f : C(X, Y))
-variable (T : DiscreteQuotient Y)
-#check (T.proj : Y → T)
-#check T.toSetoid
+/-
+  apply to F = cst X, G st lim G = Y.diagram and f : X ⟶ Y in Profinite
+-/
 
 
-#check T
+/-
+  if f is injective, then X ⟶ im is limit cone
+-/
 
-
-
-
-
-
-
-
-
-
-
-
-#print inferInstance
-
-
-open MonoFactorisation
-
-def mf := MonoFactorisation f
-#check (mf X Y f)
-
-#check mf.I
-#check mf.e
-#check mf.m
-#check mf.fac
-
-
-
-
-
-
-
-
-
-
-
-
-
--- underlying discrete space of S
-
-
-
-
--- implement range of f
-def fund : X → Y := f
-#check f
-#check (f: X→ Y)
-
-def range (f : X ⟶ Y) : Set Y := { y | ∃ x, f x = y }
-
-
-variable (S T : DiscreteQuotient Y) (g : S ⟶ T)
-#check S
-#check g
-#check (S : Type u)
-
-
-
-
--- SCRATCHPAD END
-
-
-/- given continuous map f : X → Y of profinite spaces,
-  with Y = lim Y_i the canonical diagram of Y,
-  produce a diagram of X with X_i = im(X → Y → Y_i)
-  and a map X → lim X_i compatible with f -/
-
-def diagram_of_injection {X Y : Profinite.{u}} (f : X → Y) (f_cont: Continuous f) :
-    DiscreteQuotient Y ⥤ Type u where
-  obj S := Set.range (S.proj ∘ f)
-  map {S T} g := λ x ↦ g (f x)
-
--- having difficulty formalizing this. TODO: find baby case, split up in smaller problems
--- minimal example?
-
-
-
-/- if moreover f is injective, then the map X → lim X_i is an iso -/
 
 
 
