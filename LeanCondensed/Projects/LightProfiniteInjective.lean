720ef8d7832126f47028c8c6f758c354e722f045
LennyTaelman <lenny.taelman@gmail.com>
2024-10-21 13:53:55 +0200

    progress on finite clopen separation

--- LeanCondensed/Projects/LightProfiniteInjective.lean
+++ LeanCondensed/Projects/LightProfiniteInjective.lean
@@ -28,102 +28,78 @@ variable (X : Type u) [TopologicalSpace X] [CompactSpace X] [T2Space X] [Totally
 
 lemma clopen_sandwich (Z U : Set X) (hZ : IsClosed Z) (hU : IsOpen U) (hZU : Z ⊆ U) :
     ∃ C : Set X, IsClopen C ∧ Z ⊆ C ∧ C ⊆ U := by
-
   -- every z ∈ Z has clopen nbhd Vz z ⊆ U
   have h_clopen_nbhd : ∀ z ∈ Z, ∃ V : Set X, IsClopen V ∧ z ∈ V ∧ V ⊆ U := by
     intro z hz
     exact compact_exists_isClopen_in_isOpen hU (hZU hz)
   choose V hV using h_clopen_nbhd
   let Vz : Z → Set X := fun z ↦ V z.val z.property
-
   -- the V z cover Z
   have h_cover : Z ⊆ Set.iUnion Vz := by
     intro z hz
     rw [Set.mem_iUnion]
     use ⟨z, hz⟩
     exact (hV z hz).2.1
-
   -- the V z are open and closed
   have h_open : ∀ z : Subtype Z, IsOpen (Vz z) := fun ⟨z, hz⟩ ↦ (hV z hz).1.2
   have h_clopen : ∀ z : Subtype Z, IsClopen (Vz z) := fun ⟨z, hz⟩ ↦ (hV z hz).1
-
   -- there is a finite subcover, let C be its union
   have h_compact := IsClosed.isCompact hZ
   choose I hI using IsCompact.elim_finite_subcover h_compact Vz h_open h_cover
   let C := ⋃ (i ∈ I), Vz i
-
   -- C is clopen
   have h_C_clopen : IsClopen C := by
     apply Set.Finite.isClopen_biUnion
     · exact Finset.finite_toSet I
     · intro i _
       exact h_clopen i
-
-  -- done; C is the desired clopen set
   exact ⟨C, h_C_clopen, by tauto, by aesop⟩
 
 
-/- let's play a bit with Fin n and its embedding in Fin n+1 first -/
 
-variable (n : ℕ)
 
-lemma test (i : Fin n) : (i : Fin (n+1)) ≠ n :=
-  Fin.ne_of_lt (Fin.natCast_strictMono (Nat.le_refl n) i.isLt)
-
-/- induction step: -- pick Zn ⊆ Cn ⊆ U disjoint from Zi with i lt n,
-  then apply induction hypothesis to Zi ⊆ X \ Cn
-  perhaps better to formulate with extra U containing all the Zn?
-
--/
 lemma fin_clopen_separation (n : ℕ) (Z : Fin n → Set X) (U : Set X)
-    (h_closed : ∀ i, IsClosed (Z i)) (h_disj : ∀ i j, i ≠ j → (Z i) ∩ (Z j) = ∅ )
-    (hU : IsOpen U) (hZU : ∀ i, Z i ⊆ U) :
+    (Z_closed : ∀ i, IsClosed (Z i)) (Z_disj : ∀ i j, i ≠ j → (Z i) ∩ (Z j) = ∅ )
+    (U_open : IsOpen U) (hZU : ∀ i, Z i ⊆ U) :
     ∃ C : Fin n → Set X, ∀ i, IsClopen (C i) ∧ Z i ⊆ C i ∧ C i ⊆ U ∧
     ∀ i j, i ≠ j → C i ∩ C j = ∅ := by
   induction' n with n ih generalizing U
   · use fun i => ∅ -- can use junk, domain is empty
     intro i; apply Fin.elim0 i
-  · -- let Z' be the restriction to Fin n
-    let Z' : Fin n → Set X := fun i ↦ Z i
-    have h_closed' : ∀ i, IsClosed (Z' i) := by tauto
-    have h_disj' : ∀ i j, i ≠ j → (Z' i) ∩ (Z' j) = ∅ := by aesop
-    have h_disj'' : ∀ i,  (Z n) ∩ (Z' i) = ∅ := by
-      intro i
-      apply h_disj
-      exact Fin.ne_of_gt (Fin.natCast_strictMono (Nat.le_refl n) i.isLt )
-    -- U' contains Zn and is disjoint from the other Zi
-    let U' : Set X  := U \ ( ⋃ (i : Fin n), Z' i)
-    have hU' : IsOpen U' := IsOpen.sdiff hU (isClosed_iUnion_of_finite h_closed')
-    have hZnU' : Z n ⊆ U' := by
-      refine Set.subset_diff.mpr ?_
+  · -- let Z' be the restriction along succ : Fin n → Fin (n+1)
+    let Z' : Fin n → Set X := fun i ↦ Z (Fin.succ i)
+    have Z'_closed : ∀ i, IsClosed (Z' i) := fun i ↦ Z_closed (Fin.succ i)
+    have Z'_disj : ∀ i j, i ≠ j → (Z' i) ∩ (Z' j) = ∅ := fun i j hij =>
+      Z_disj (Fin.succ i) (Fin.succ j) (fun h ↦ hij (Fin.succ_inj.1 h))
+    -- find Z0 ⊆ V ⊆ U disjoint from the Zi with i>0
+    let V : Set X  := U \ (⋃ (i : Fin n), Z' i)
+    have V_open : IsOpen V := IsOpen.sdiff U_open (isClosed_iUnion_of_finite Z'_closed)
+    have Z0_in_V : Z 0 ⊆ V := by
+      apply Set.subset_diff.mpr
       constructor
-      · exact hZU n
-      · apply Set.disjoint_iUnion_right.2
+      · exact hZU 0
+      · apply Set.disjoint_iUnion_right.mpr
         intro i
-        exact Set.disjoint_iff_inter_eq_empty.mpr (h_disj'' i)
-    -- pick Zn ⊆ Cn ⊆ U'
-    choose Cn hcn using clopen_sandwich X (Z n) U' (h_closed n) hU' (hZnU')
-    let U'' : Set X := Cnᶜ
-    have Cn_closed : IsClosed (Cn) := hcn.1.1
-    have U''_open : IsOpen U'' := IsClosed.isOpen_compl
-    have hZU'' : ∀ i : Fin n, Z' i ⊆ U'' := by --- uh FALSE??
+        apply Set.disjoint_iff_inter_eq_empty.mpr
+        apply Z_disj
+        exact Ne.symm (Fin.succ_ne_zero i)
+    have V_disj_Z' : ∀ i : Fin n, Disjoint V (Z' i) := by
       intro i
-      apply Disjoint.subset_compl_right
-      -- use that Cn is contained in U' by construction
-      have hCnU' : Cn ⊆ U' := hcn.2.2
-
-
-
-      sorry
-
-
-    -- now use induction hypothesis on Z' and U''
-    choose C' hC' using ih Z' U'' h_closed' h_disj' U''_open hZU''
-
-
-
-
-
+      have h : Z i.succ ⊆ ⋃ (i : Fin n), Z (Fin.succ i) := Set.subset_iUnion_of_subset i fun ⦃a⦄ a ↦ a
+      exact Disjoint.mono_right h Set.disjoint_sdiff_left
+    -- pick clopen Z0 ⊆ C0 ⊆ V
+    choose C0 hC0 using clopen_sandwich X (Z 0) V (Z_closed 0) V_open Z0_in_V
+    -- now let W be the complement of C0,
+    let W : Set X := C0ᶜ
+    have W_open : IsOpen W := isOpen_compl_iff.mpr hC0.1.1
+    have Z'_in_W : ∀ i : Fin n, Z' i ⊆ W := by
+      intro i
+      apply Disjoint.subset_compl_left
+      exact Disjoint.mono_left hC0.2.2 (V_disj_Z' i)
+    -- use induction hypothesis to choose Z i ⊆ Ci ⊆ W clopen and mutually disjoint for i>0
+    choose Ci hCi using ih Z' W Z'_closed Z'_disj W_open Z'_in_W
+    -- now define C succ i = Ci i and C 0 = C0
+    -- bah, disjointness might be a messy case distinction. Maybe change all ≠ to < ???
 
 
     sorry
@@ -131,50 +107,13 @@ lemma fin_clopen_separation (n : ℕ) (Z : Fin n → Set X) (U : Set X)
 
 
 
--- perhaps do finite version first
-lemma fin_clopen_separation' (I : Type) [h_fin: Finite I] (Z : I → Set X)
-    (h_closed : ∀ i, IsClosed (Z i))
-    (h_disj : ∀ i j, i ≠ j → (Z i) ∩ (Z j) = ∅ ) :
-    ∃ C : I → Set X, ∀ i, IsClopen (C i) ∧ Z i ⊆ C i ∧ ∀ j ≠ i, C i ∩ Z j = ∅ := by
-  let compl : I → Type := fun i ↦ {j : I // j ≠ i}
-  have h_compl : ∀ i, Finite (compl i) := by
-    intro i
-    exact Set.toFinite _
-  let Z' : I → Set X := fun i ↦ ⋃ j : {j : I // j ≠ i}, Z j
-  let U : I → Set X := fun i ↦ (Z' i)ᶜ
-  have hZ' : ∀ i, IsClosed (Z' i) := by
-    intro i
-    refine isClosed_iUnion_of_finite ?h
-    intro j
-    exact h_closed j.1
-  have hU : ∀ i, IsOpen (U i) := fun i ↦ IsClosed.isOpen_compl
-  have hZU : ∀ i, Z i ⊆ U i := by
-    intro i z hz
-    apply Set.mem_compl
-    unfold Z'
-    rw [Set.mem_iUnion]
-    push_neg
-    intro j
-    have hd := h_disj j.val i j.property
-    intro h2
-    have h3 : z ∈ Z j.val ∩ Z i := ⟨ h2, hz ⟩
-    simp_all only [ne_eq, Set.compl_iUnion, Set.mem_empty_iff_false, compl, Z', U]
-  -- now can apply previous lemma
-
-
-
-
-
-
-  sorry
-
 -- can now prove key extension lemma for functions to nonempty finite sets
 
 lemma to_discrete_lifts_along_injective_profinite
-  (S : Type u) [TopologicalSpace S] [DiscreteTopology S] [Nonempty S] [fin : Finite S]
-  (X Y : Profinite.{u}) (f : X → Y) (f_cont: Continuous f) (f_inj: Function.Injective f)
-  (g : X → S) (g_cont : Continuous g) :
-  ∃ h : Y → S, (h ∘ f = g) ∧ (Continuous h) := by
+    (S : Type u) [TopologicalSpace S] [DiscreteTopology S] [Nonempty S] [fin : Finite S]
+    (X Y : Profinite.{u}) (f : X → Y) (f_cont: Continuous f) (f_inj: Function.Injective f)
+    (g : X → S) (g_cont : Continuous g) :
+    ∃ h : Y → S, (h ∘ f = g) ∧ (Continuous h) := by
   -- let Z s be the inverse image of s
   let Z : S → Set X := fun s ↦ g ⁻¹' {s}
   have hZ : ∀ s, IsClosed (Z s) := by
@@ -191,6 +130,7 @@ lemma to_discrete_lifts_along_injective_profinite
   have hU : ∀ s, IsOpen (U s) := by
     intro s
     exact IsClosed.isOpen_compl
+  sorry
 
   -- write Y as lim Y_i with Y_i discrete
 
